cb54e78bcb897aee6aed75a1e4cc50f99947815f
JiaxianGu <gujiaxian1997@gmail.com>
2024-03-08 16:27:25 +0900

    fix: add props user, isLoggedIn in App.tsx, Route path=/:username/:projectId

--- hone/src/routes/Project.tsx
+++ hone/src/routes/Project.tsx
@@ -1,10 +1,19 @@
 import { FC } from "react";
 import "../styles/project.css"
 import { useParams } from "react-router-dom";
+import { User } from '../globals';
 
-const Project: FC = () => {
+
+type Props = {
+  user: User | null;
+  isLoggedIn: boolean;
+}
+
+const Project: FC<Props> = ({ user, isLoggedIn }) => {
   const { username, projectId} = useParams<string>();
-  
+  const [viewerUserName, setViewerUserName] = useState<string>(username);
+  const [projectOwnerName, setProjectOwnerName] = useState<string>("");
+
   return (
     <>
 
