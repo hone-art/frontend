b42c24348fe3a06be1a10c078ef328be39dae4f1
JiaxianGu <gujiaxian1997@gmail.com>
2024-03-10 17:39:54 +0900

    feat: create new entry in project page. Project.tsx

--- hone/src/routes/Project.tsx
+++ hone/src/routes/Project.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from "react";
+import { FC, useState, useEffect, useRef } from "react";
 import "../styles/project.css"
 import { useParams, Link } from "react-router-dom";
 import { User, Image } from '../globals';
@@ -8,10 +8,23 @@ import LoggedOutHeader from "../components/LoggedOutHeader";
 import ProjectDescription from "../components/ProjectDescription";
 import EditableProjectDescription from "../components/EditableProjectDescription";
 import Entry from "../components/Entry";
+import { storage } from '../firebase';
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 // import { parse } from "dotenv";
 
 // const BACKEND_URL = 'https://hone-backend-6c69d7cab717.herokuapp.com';
 const BACKEND_URL = 'http://localhost:8080';
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure
+} from '@chakra-ui/react'
 
 
 type Props = {
@@ -26,6 +39,14 @@ const Project: FC<Props> = ({ user, isLoggedIn }) => {
   const [projectImageURL, setProjectImageURL] = useState<string>("");
   const [isProjectEditable, setIsProjectEditable] = useState<boolean>(false);
   const [entries, setEntries] = useState<Array<EntryInterface>>();
+  const [newEntryImage, setNewEntryImage] = useState<File>();
+  const [newEntryImageId, setNewEntryImageId] = useState<string>("");
+  const [newEntryDescription, setNewEntryDescription] = useState<string>("");
+  const [currentProjectUserId, setCurrentProjectUserId] = useState<number>();
+
+  const { isOpen, onOpen, onClose } = useDisclosure(); //Modal
+
+  const inputImage = useRef(null); //User upload new entry photo
 
   useEffect(() => {
     if (user?.user_name === username) setIsSameUser(true);
@@ -45,7 +66,14 @@ const Project: FC<Props> = ({ user, isLoggedIn }) => {
       setEntries(entries);
     }
 
+    async function fetchCurrentProjectUserId() {
+      const fetchResult = await fetch(`${BACKEND_URL}/projects/${projectId}`);
+      const currentProjectObj = await fetchResult.json();
+      setCurrentProjectUserId(currentProjectObj.user_id);
+    }
+
     fetchProjectAndEntries();
+    fetchCurrentProjectUserId();
   }, [])
 
 
@@ -59,17 +87,78 @@ const Project: FC<Props> = ({ user, isLoggedIn }) => {
     // })
   }
 
+  async function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const imageToUpload = event.target.files![0];
+    setNewEntryImage(imageToUpload);
+    console.log(imageToUpload);
+  }
+
+  async function handleCreateNewEntry() {
+    if (newEntryImage) {
+      const storageRef = ref(storage, `${newEntryImage.name}`);
+      const snapshot = await uploadBytes(storageRef, newEntryImage);
+      const imgUrl = await getDownloadURL(snapshot.ref);
+
+      const newImageBody = {url: imgUrl};
+      const fetchNewEntryImage = await fetch(`${BACKEND_URL}/images`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newImageBody)
+      });
+
+      const newImage = await fetchNewEntryImage.json();
+      setNewEntryImageId(newImage.id);
+    }
+
+    const newEntryBody = {
+      description: newEntryDescription, 
+      img_id: newEntryImageId,
+      project_id: Number(projectId),
+      user_id: currentProjectUserId
+    }
+    const createNewEntryResponse = await fetch(`${BACKEND_URL}/entries/`, {
+      method: 'POST',
+      headers: {"Content-Type": "application/json",},
+      body: JSON.stringify(newEntryBody),
+    })
+    onClose();
+  }
+
   return (
     <>
       {isLoggedIn ? <LoggedInHeader user={user} /> : <LoggedOutHeader />}
       <section className="project-container">
         <Link to={`/${username}`}>← Back</Link>
         {isProjectEditable ? <EditableProjectDescription project={project} setProject={setProject} projectImageURL={projectImageURL} setProjectImageURL={setProjectImageURL} setIsProjectEditable={setIsProjectEditable} /> : <ProjectDescription project={project} projectImageURL={projectImageURL} isSameUser={isSameUser} setIsProjectEditable={setIsProjectEditable} />}
-        {isSameUser ? <button onClick={handleOnClick}>+ Create new entry</button> : null}
+        {isSameUser ? <button onClick={onOpen}>+ Create new entry</button> : null}
         {entries?.map((entry) => (
           <Entry entry={entry} key={entry.id} isSameUser={isSameUser} />
         ))}
       </section>
+      <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Create new entry</ModalHeader>
+            <ModalCloseButton className="modal-close-btn" />
+            <ModalBody>
+              <p className="margin-bottom">Entry image:</p>
+              <input type="file" className="margin-bottom" ref={inputImage} onChange={handleChange} accept="image/*" />
+              <p className="margin-bottom">Entry description: </p>
+              <input type="text" className="input-name" value={newEntryDescription} onChange={(e) => setNewEntryDescription(e.target.value)} />
+            </ModalBody>
+
+            <ModalFooter className="modal-footer">
+            <div className="btn-container">
+              <button className="modal-btn" onClick={onClose}>
+                Cancel
+              </button>
+              <button className="modal-btn" onClick={handleCreateNewEntry}>Create</button>
+            </div>
+          </ModalFooter>
+          </ModalContent>
+      </Modal>
     </>
   )
 };
