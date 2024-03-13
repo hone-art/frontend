1cecd6312450931f8e5c3d1225b175d734a0c520
JiaxianGu <gujiaxian1997@gmail.com>
2024-03-13 12:36:44 +0900

    feat: add Heatmap component to profile page

---
+++ hone/src/components/Heatmap.tsx
@@ -0,0 +1,11 @@
+const Heatmap = () => {
+    return (
+        <div>
+            hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello
+            hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello
+
+        </div>
+    )
+}
+
+export default Heatmap;
