539454187d3a299082fdb181a0863e332ff606dd
yurikahirata <yurikahirata0130@gmail.com>
2024-03-15 11:51:58 +0900

    fix: signup cookies

--- hone/src/routes/Profile.tsx
+++ hone/src/routes/Profile.tsx
@@ -198,7 +198,7 @@ const Profile: FC = () => {
           <h1 id="display-name">{userProfile?.display_name}</h1>
           <h2 id="username">@{userProfile?.user_name}</h2>
           {isUser ? <button className="edit-profile-btn" onClick={onOpen}>Edit profile</button> : null}
-          <Heatmap></Heatmap>
+          <Heatmap isUser={isUser}></Heatmap>
         </div>
         <div className="projects-container">
           {isUser ? <button className="new-project-btn" onClick={handleNewProjectOnClick}>+ Create new project</button> : null}
